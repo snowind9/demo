0efac1cd7d1b8459d123f87b4c6a1a85d6659078
snowind9 <snowind9@163.com>
2016-06-01 09:40:35 +0800

    change some UI

--- bootstrap/src/main/resources/static/js/dashboard.js
+++ bootstrap/src/main/resources/static/js/dashboard.js
@@ -4,17 +4,33 @@ $(document).ready(function () {
         e.preventDefault();
         
         var $this = $(this);
-    
+        var table = $('#example').DataTable();
+       
         if ($this.children('i').hasClass('glyphicon-resize-full'))
         {
             $this.children('i').removeClass('glyphicon-resize-full');
             $this.children('i').addClass('glyphicon-resize-small');
+            $('.dataTables_scrollBody').css("max-height","1000px");
         }
         else if ($this.children('i').hasClass('glyphicon-resize-small'))
         {
             $this.children('i').removeClass('glyphicon-resize-small');
             $this.children('i').addClass('glyphicon-resize-full');
+            $('.dataTables_scrollBody').css("max-height","350px");
         }
         $(this).closest('.panel').toggleClass('panel-fullscreen');
+        table.columns.adjust().draw();
+        
     });
+    
+    $('#example').DataTable({
+        fixedHeader:    true,
+    	scrollY:       	350,
+        scrollCollapse: true,
+        paging:         false,
+        searching:         false,
+        "info": false
+    });
+    
+    $('[data-toggle="tooltip"]').tooltip();
 });
